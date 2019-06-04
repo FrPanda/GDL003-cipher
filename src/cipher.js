d36d3c5a60a770bef91a93c9b9855543d25da839
FRANCIS SANTOYO <francis_3991@hotmail.com>
2019-06-03 19:09:52 -0500

    Avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,9 @@
-window.cipher = {
-  // ... 
+  // ..const textoCifrado = document.getElementById("cifrado");
+cipher.encode(offset)
+cipher.decode(offset)
+  function cifrado(){
+  	const textoIngresado = texto.value;
+  	textoCifrado.value = textoIngresado.split('').map(c=>{
+  		let mayus = (c === c.toUpperCase()) ? true;
+		}
 };
